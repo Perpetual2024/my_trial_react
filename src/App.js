fbec30966718ffd9d184a826ff839634237a64b7
Perpetual Akinyi <you@example.com>
2026-03-19 18:13:08 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import {BrowserRouter as Route,Router,Routes} from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import Aboutpage from './pages/Aboutpage'
+import Categories from './pages/Categories'
+
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+   <Router>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<Aboutpage/>}/>
+      <Route path="/category" element={<Categories/>}/>
+    </Routes>
+   </Router>
   )
 }
 
